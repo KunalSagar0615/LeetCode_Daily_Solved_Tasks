f34ac11ae795bffba97b0d8cd0eaf655864dd29e
Kunal Ananda Sagar <kunalsagar0615@gmail.com>
2026-08-07 14:14:58 +0530

    Time: 42 ms (61.24%), Space: 53.4 MB (69.89%) - LeetHub

---
+++ 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
@@ -0,0 +1,15 @@
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function* () {
+    let a = 0;
+    let b = 1;
+
+    while (true) {
+        yield a;
+
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+};
